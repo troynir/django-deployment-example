c7780fed942269344659c9293d4e67b3040f2fe4
troynir <82532813+troynir@users.noreply.github.com>
2021-04-17 16:51:29 +0530

    Update my_extras.py

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -1,13 +1,13 @@
 from django import template
 
-'''
-register = template.Library()
-@register.filter(name='cut')
-def cut(value,arg):
+
+#register = template.Library()
+#@register.filter(name='cut')
+#def cut(value,arg):
     '''
     Cuts out alll val of arg from string
     '''
-    return.replace(arg,'')
+    #return.replace(arg,'')
 
 #register.filter('cut',cut)
-'''
+
